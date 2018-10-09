e8e7cbdc84e6d9c263fa628138cef2d273d895dd
jeffreypang911 <jeffreypang911@gmail.com>
2018-10-09 22:54:58 +0000

    adds complteted exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,3 +4,16 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+
+
+@store1 = Store.find_by(id: 1)
+store1.id = @store1
+store1.save
+
+@store2 = Store.find_by(id: 2)
+store2.id = @store2
+store2.save
+
+@store3 = Store.find_by(name: "Burnaby")
+store3.name = "Burnaby1"
+store3.save
